9c425329279a0b4bb06be5a32f0c3fee267916a8
v4l3-v1ll4rr03l <villarroelvalentina@MacBook-Air-6.attlocal.net>
2023-10-14 18:13:13 -0400

    Restructuring files

--- decade_playlists/playlistProcessing.py
+++ decade_playlists/playlistProcessing.py
@@ -1,25 +1,24 @@
 import json
 
-def processDecade(curr_data):
-    id_str = ''
-    i = 0
-    song_dict = {}
-    for item in curr_data['tracks']['items']:
-        if i < 100:
-            id_str += str(item['track']['id']) + ", "
-            song_dict[str(item['track']['id'])] = str(item['track']['name'])
-        i += 1
-    print(id_str)
-    return song_dict
-
-
-decade_dict = {}
-decade_files = ["the1950s.json", 'the1960s.json', 'the1970s.json', 'the1980s.json', 'the1990s.json', 'the2000s.json']
-decades = ["1950s", '1960s', '1970s', '1980s', '1990s', '2000s']
+decade_data =[]
+decade_files = ["decade_playlists/the1950s.json", 'decade_playlists/the1960s.json', 'decade_playlists/the1970s.json', 'decade_playlists/the1980s.json', 'decade_playlists/the1990s.json', 'decade_playlists/the2000s.json']
+decades = [1950, 1960, 1970, 1980, 1990, 2000]
 for i in range(len(decades)):
     curr_file = open(decade_files[i])
     curr_data = json.load(curr_file)
-    print("\n" + decades[i])
-    decade_dict[decades[i]] = processDecade(curr_data)
+    id_str = ''
+    j = 0
+    song_dict = {}
+    for item in curr_data['tracks']['items']:
+        print("\n" + str(decades[i])+"s")
+        if j < 100:
+            id_str += str(item['track']['id']) + ", "
+            song_dict['id'] = str(item['track']['id'])
+            song_dict['name'] = str(item['track']['name'])
+            song_dict['decade'] = decades[i]
+            decade_data.append(song_dict)
+        j += 1
+        print(id_str)
+
 with open("decades_dict.json", "w") as outfile:
-    json.dump(decade_dict, outfile)
+    json.dump(decade_data, outfile)
